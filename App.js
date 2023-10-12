262c8503737f7b96a7d878649ec71233d7fa0c43
virushan-510 <120415400+virushan-510@users.noreply.github.com>
2023-10-12 23:49:13 +0530

    react Notepad

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { LoginUi } from "./Login";
+import { RegisterUi } from "./Register";
+import { HomeUi } from "./Home";
+import { LoginHomeUi } from "./LoginHome";
+import { CreateNoteUi } from "./CreateNote";
+import { AllNotesUi } from "./AllNotes";
+import { ViewNoteUi } from "./ViewNote";
+import { ViewAllNotesUi } from "./ViewAllNotes";
+import LoadingScreen from "./LoadingScreen";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const [initialRoute, setInitialRoute] = useState("Home");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const mobile = await AsyncStorage.getItem("mobile");
+        if (mobile) {
+          setInitialRoute("LoginHome");
+        } else {
+          setInitialRoute("Home");
+        }
+      } catch (error) {
+        console.error("Error checking login status: ", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  if (isLoading) {
+    // Show the loading screen while checking login status
+    return <LoadingScreen />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={initialRoute}>
+        <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeUi} />
+        <Stack.Screen name="Login" component={LoginUi} />
+        <Stack.Screen name="Register" component={RegisterUi} />
+        <Stack.Screen options={{ headerShown: false }} name="LoginHome" component={LoginHomeUi} />
+        <Stack.Screen options={{ headerShown: false }} name="CreateNote" component={CreateNoteUi} />
+        <Stack.Screen options={{ headerShown: false }} name="AllNotes" component={AllNotesUi} />
+        <Stack.Screen options={{ headerShown: false }} name="ViewNote" component={ViewNoteUi} />
+        <Stack.Screen options={{ headerShown: false }} name="ViewAllNotes" component={ViewAllNotesUi} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
